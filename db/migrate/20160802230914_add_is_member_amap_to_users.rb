b9874672012d70e14f253ca196c0ee099cc55c9b
maichomper <maichomper@gmail.com>
2016-08-03 13:40:51 -0500

    change is_member_ampa default to false in migration

--- db/migrate/20160802230914_add_is_member_amap_to_users.rb
+++ db/migrate/20160802230914_add_is_member_amap_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsMemberAmapToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_member_amap, :boolean, after: :email, default: 0
+    add_column :users, :is_member_amap, :boolean, after: :email, default: false
   end
 end
