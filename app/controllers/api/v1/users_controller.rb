8b839c61f51144be3d923550f83dfe06003aa0e2
maichomper <maichomper@gmail.com>
2016-08-08 23:49:34 -0500

    add missing endpoints for something

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -45,6 +45,7 @@ module Api::V1
         render json: { success: 'Se ha enviado un correo con instrucciones para restablecer contraseña' }, status: 200, location: [:api, user]
         return
       end
+      user = User.new
       user.errors.add(:email, "No existe ningún usuario con ese email")
       render json: { errors: user.errors }, status: :unprocessable_entity
     end
