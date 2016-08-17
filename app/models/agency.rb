ed54e39e0324510cc0057720f3f734c21e591b2f
maichomper <maichomper@gmail.com>
2016-08-17 18:52:32 -0500

    adds agency path to agency model storage files

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,7 +1,7 @@
 class Agency < ApplicationRecord
   has_many :users
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "200x200#" }, default_url: "/images/:style/missing.png", :path => ":rails_root/storage/#{Rails.env}#{ENV['RAILS_TEST_NUMBER']}/attachments/:id/:style/:basename.:extension", :url => ":rails_root/storage/#{Rails.env}#{ENV['RAILS_TEST_NUMBER']}/attachments/:id/:style/:basename.:extension"
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "200x200#" }, default_url: "/images/:style/missing.png", :path => ":rails_root/storage/agency/#{Rails.env}#{ENV['RAILS_TEST_NUMBER']}/attachments/:id/:style/:basename.:extension", :url => ":rails_root/storage/#{Rails.env}#{ENV['RAILS_TEST_NUMBER']}/attachments/:id/:style/:basename.:extension"
   
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
   validates :name, uniqueness: true
