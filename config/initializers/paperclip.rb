dc1b73e4661acef926016521aad1326a4031da2c
maichomper <maichomper@gmail.com>
2016-08-17 19:03:31 -0500

    fix aws region for production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,11 +2,11 @@ if Rails.env.development? || Rails.env.test?
   Paperclip::Attachment.default_options[:storage] = 'filesystem'
 else
   Paperclip::Attachment.default_options[:storage] = 's3'
-  Paperclip::Attachment.default_options[:region] = ENV['AWS_REGION']
   Paperclip::Attachment.default_options[:s3_credentials] = {
     bucket: ENV['AWS_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['AWS_REGION']
   }
   # other config...
 end
