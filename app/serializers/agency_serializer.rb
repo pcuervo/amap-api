c361417ae5751d14e3117e9f2e71a61900a5b683
maichomper <maichomper@gmail.com>
2016-08-26 14:09:06 -0500

    formats json response for agency to include image inside success_cases

--- app/serializers/agency_serializer.rb
+++ app/serializers/agency_serializer.rb
@@ -1,3 +1,17 @@
 class AgencySerializer < ActiveModel::Serializer
   attributes :id, :name, :phone, :contact_name, :contact_email, :address, :latitude, :longitude, :website_url, :num_employees, :golden_pitch, :silver_pitch, :high_risk_pitch, :medium_risk_pitch, :logo, :success_cases, :skills
+
+  def success_cases
+    success_cases = []
+    object.success_cases.each do |sc|
+      success_case = {}
+      success_case[:name]         = sc.name
+      success_case[:description]  = sc.description
+      success_case[:url]          = sc.url
+      success_case[:case_image]   = sc.case_image
+      success_cases.push( success_case )
+    end
+    success_cases
+  end
 end
+
