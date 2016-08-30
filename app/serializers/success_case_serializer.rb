a9cbd7398f61ae34fcc306e59656db19fe707d71
maichomper <maichomper@gmail.com>
2016-08-30 15:16:08 -0500

    new thumb size for SuccessCases

--- app/serializers/success_case_serializer.rb
+++ app/serializers/success_case_serializer.rb
@@ -1,3 +1,9 @@
 class SuccessCaseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :case_image, :url
+  attributes :id, :name, :description, :case_image, :url, :case_image_thumb
+
+  def case_image_thumb
+    object.case_image(:thumb)
+  end
+
 end
+
