f10402ad957ba9f083ca7ddcc6b2a29841ff9e54
maichomper <maichomper@gmail.com>
2016-09-12 13:16:54 -0500

    add brand_id to user sessions response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :role, :is_member_amap, :auth_token, :agency_id
+  attributes :id, :first_name, :last_name, :email, :role, :is_member_amap, :auth_token, :agency_id, :brand_id
 
   def agency_id
     return -1 if object.agencies.empty?
-
     object.agencies.first.id
+  end
+
+  def brand_id
+    return -1 if object.brands.empty?
+    object.brands.first.id
   end
 end
