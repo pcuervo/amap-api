a618050f41bebbed024f42a1377d594331a7e4a7
maichomper <maichomper@gmail.com>
2016-09-27 19:34:21 -0500

    more tests error with heroku

---
+++ app/controllers/api/v1/pitch_evaluations_controller.rb
@@ -0,0 +1,43 @@
+πclass Api::V1::PitchEvaluationsController < ApplicationController
+  before_action :set_pitch_evaluation, only: [:show, :update]
+  before_action only: [:create, :update] do 
+    authenticate_with_token! params[:auth_token]
+  end
+
+   # POST /pitch_evaluations
+  def create
+    @pitch_evaluation = PitchEvaluation.new(pitch_evaluation_params)
+    @pitch_evaluation.user_id = current_user.id
+
+    if @pitch_evaluation.save
+      @pitch_evaluation.calculate_score
+      render json: @pitch_evaluation, status: :created
+      return
+    end
+
+    render json: { errors: @pitch_evaluation.errors },status: :unprocessable_entity
+  end
+
+  # POST /pitch_evaluations/update/1
+  def update
+    if ! @pitch_evaluation.present? 
+      render json: { errors: 'No se encontró la evaluación del pitch con id: ' + params[:id] },status: :unprocessable_entity
+      return
+    end
+    
+    if @pitch_evaluation.update(pitch_evaluation_params)
+      render json: @pitch_evaluation, status: :ok
+      return 
+    end
+    render json: { errors: @pitch_evaluation.errors }, status: :unprocessable_entity
+  end
+
+  # private
+  #   def set_pitch_evaluation
+  #     @pitch_evaluation = PitchEvaluation.find_by_id(params[:id])
+  #   end
+
+  #   def pitch_evaluation_params
+  #     params.require(:pitch_evaluation).permit( :pitch_id, :evaluation_status, :pitch_status, :are_objectives_clear, :time_to_present, :is_budget_known, :number_of_agencies, :are_deliverables_clear, :is_marketing_involved, :time_to_know_decision, :deliver_copyright_for_pitching, :know_presentation_rounds, :number_of_rounds, :score, :activity_status, :was_won, :has_selection_criteria )
+  #   end
+end
