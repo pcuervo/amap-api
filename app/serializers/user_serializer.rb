c0b5babd74e59936e5b9f979b398743b3d2fca56
maichomper <maichomper@gmail.com>
2016-10-03 17:41:50 -0500

    Change brands for companies in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :role, :is_member_amap, :auth_token, :agency_id, :brand_id
+  attributes :id, :first_name, :last_name, :email, :role, :is_member_amap, :auth_token, :agency_id, :company_id
 
   def agency_id
     return -1 if object.agencies.empty?
     object.agencies.first.id
   end
 
-  def brand_id
-    return -1 if object.brands.empty?
-    object.brands.first.id
+  def company_id
+    return -1 if object.companies.empty?
+    object.companies.first.id
   end
 end
