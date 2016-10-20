8a2d164175e2c425b9cba11e7534b5a797dd13fc
maichomper <maichomper@gmail.com>
2016-10-20 15:00:32 -0500

    Change password reset url

--- db/migrate/20161017232703_create_pitch_results.rb
+++ db/migrate/20161017232703_create_pitch_results.rb
@@ -3,11 +3,11 @@ class CreatePitchResults < ActiveRecord::Migration[5.0]
     create_table :pitch_results do |t|
       t.references :agency, foreign_key: true
       t.references :pitch, foreign_key: true
-      t.boolean :was_proposal_presented, default: false
-      t.boolean :got_response, default: true
-      t.boolean :was_pitch_won, default: true
-      t.boolean :got_feedback, default: true
-      t.boolean :has_someone_else_won, default: true
+      t.boolean :was_proposal_presented
+      t.boolean :got_response
+      t.boolean :was_pitch_won
+      t.boolean :got_feedback
+      t.boolean :has_someone_else_won
       t.date :when_will_you_get_response
       t.date :when_are_you_presenting
 
