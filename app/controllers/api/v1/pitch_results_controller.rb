a4abf5de184e760cd18f40671d580928fee67e17
maichomper <maichomper@gmail.com>
2016-10-19 21:59:07 -0500

    adds endpoint to create PitchResults

--- app/controllers/api/v1/pitch_results_controller.rb
+++ app/controllers/api/v1/pitch_results_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::PitchResultsController < ApplicationController
+  before_action only: [:create] do 
+    authenticate_with_token! params[:auth_token]
+  end
+
+  # POST /pitch_results
+  def create
+    @pitch_result = PitchResult.new(pitch_result_params)
+    
+    if @pitch_result.save
+      render json: @pitch_result, status: :created
+      return
+    end
+
+    render json: { errors: @pitch_result.errors },status: :unprocessable_entity
+  end
+
+  private
+    # Only allow a trusted parameter "white list" through.
+    def pitch_result_params
+      params.require(:pitch_result).permit( :agency_id, :pitch_id, :was_proposal_presented, :got_response, :was_pitch_won, :got_feedback, :has_someone_else_won, :when_will_you_get_response, :when_are_you_presenting )
+    end
 end
--- spec/controllers/api/v1/pitch_results_controller_spec.rb
+++ spec/controllers/api/v1/pitch_results_controller_spec.rb
@@ -1,23 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PitchResultsController, :type => :controller do
-  describe "GET #show" do
-    before(:each) do
-      api_key = ApiKey.create
-      api_authorization_header 'Token ' + api_key.access_token
-      @pitch_result = FactoryGirl.create :pitch_result
-      get :show, params: { id: @pitch_result.id }, format: :json
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        api_key = ApiKey.create
+        api_authorization_header 'Token ' + api_key.access_token
+        @admin = FactoryGirl.create :user
+
+        agency = FactoryGirl.create :agency
+        pitch = FactoryGirl.create :pitch
+        @pitch_result_attributes = FactoryGirl.attributes_for :pitch_result
+        @pitch_result_attributes[:agency_id] = agency.id
+        @pitch_result_attributes[:pitch_id] = pitch.id
+        post :create, params: { auth_token: @admin.auth_token, pitch_result: @pitch_result_attributes }, format: :json
+      end
+
+      it "renders the json representation for the PitchResult just created" do
+        pitch_result_response = json_response
+        expect(pitch_result_response[:agency_id]).to eql @pitch_result_attributes[:agency_id]
+      end
+
+      it { should respond_with 201 }
     end
 
-    it "returns the information about a Pitch on a hash" do
-      expect(json_response[:was_proposal_presented]).to eql @pitch_result.was_proposal_presented
-    end
+    context "when is not created" do
+      before(:each) do
+        api_key = ApiKey.create
+        api_authorization_header 'Token ' + api_key.access_token
+        @admin = FactoryGirl.create :user
 
-    it "returns the Agency of the PitchResult" do 
-      pitch_result_response = json_response
-      expect(pitch_result_response).to have_key(:agency)
-    end
+        @pitch_result_attributes = FactoryGirl.attributes_for :pitch_result
+        puts @pitch_result_attributes.to_json
+        post :create, params: { auth_token: @admin.auth_token, pitch_result: @pitch_result_attributes }, format: :json
+      end
 
-    it { should respond_with 200 }
-  end
+      it "includes an errors key" do
+        pitch_result_response = json_response
+        expect(pitch_result_response).to have_key(:errors)
+      end
+
+      it { should respond_with 422 }
+    end
+  end #POST create
+
 end
