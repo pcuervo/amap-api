c459857ea253c1e0abadc3ee81fd281449c57d60
maichomper <maichomper@gmail.com>
2016-11-01 13:43:41 -0600

    increase requests per second in Rack Attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,7 +10,7 @@ class Rack::Attack
   end
 
   # Allow an IP address to make 5 requests every 5 seconds
-  throttle('req/ip', limit: 5, period: 5) do |req|
+  throttle('req/ip', limit: 50, period: 50) do |req|
     req.ip
   end
 
