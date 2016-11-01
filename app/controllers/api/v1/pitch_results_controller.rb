dee2cb21eea266d2e30d3f96ac170dd6dcf42866
maichomper <maichomper@gmail.com>
2016-11-01 12:51:54 -0600

    save status if Pitch was won or not after adding results

--- app/controllers/api/v1/pitch_results_controller.rb
+++ app/controllers/api/v1/pitch_results_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PitchResultsController < ApplicationController
   before_action only: [:create] do 
     authenticate_with_token! params[:auth_token]
   end
-  after_action :update_evaluation_to_won, only: [:create], if: -> { @was_won }
+  after_action :update_evaluation_won_status, only: [:create], if: -> { @was_won.present? }
 
   # POST /pitch_results
   def create
@@ -23,14 +23,14 @@ class Api::V1::PitchResultsController < ApplicationController
       params.require(:pitch_result).permit( :agency_id, :pitch_id, :was_proposal_presented, :got_response, :was_pitch_won, :got_feedback, :has_someone_else_won, :when_will_you_get_response, :when_are_you_presenting )
     end
 
-    def update_evaluation_to_won
+    def update_evaluation_won_status
       agency = Agency.find( @pitch_result.agency_id )
       user_ids = []
       agency.users.each do |user|
         user_ids.push( user.id )
       end
       pitch_evaluation = PitchEvaluation.where( 'user_id in (?) AND pitch_id = ?', user_ids, @pitch_result.pitch_id ).first
-      pitch_evaluation.was_won = true 
+      pitch_evaluation.was_won = @was_won 
       pitch_evaluation.save
     end
 end
