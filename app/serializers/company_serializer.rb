a8612a7523efb7354e5c0fe2bff03de26ede4547
maichomper <maichomper@gmail.com>
2017-04-05 21:11:41 -0500

    Add pitches and users to CompanySerializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,5 +1,5 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :brands, :contact_name, :contact_email, :contact_position, :logo, :favorite_agencies, :users
+  attributes :id, :name, :brands, :contact_name, :contact_email, :contact_position, :logo, :favorite_agencies, :users, :pitches
 
   def favorite_agencies
     favs = []
@@ -7,5 +7,29 @@ class CompanySerializer < ActiveModel::Serializer
       favs.push({:id => fav.id, :name => fav.name})
     end
     favs
+  end
+
+  def pitches
+    pitches = []
+    object.brands.each do |brand|
+      brand.pitches.each do |pitch|
+        pitches.push({
+          :id             => pitch.id,
+          :name           => pitch.name,
+          :brand          => brand,
+          :brief_date     => pitch.brief_date,
+          :email_contact  => pitch.brief_email_contact
+        })
+      end
+    end
+    pitches
+  end
+
+  def users
+    users = []
+    object.users.each do |u|
+      users.push({:email => u.email})
+    end
+    users
   end
 end
