97dcf855c7e31ecfa4b6667ad685b869a919adc9
maichomper <maichomper@gmail.com>
2017-07-20 20:23:02 -0500

    Add method to load companies and brands with CSV

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+  require 'csv'
+
   has_and_belongs_to_many :users
   has_many :brands
   has_and_belongs_to_many :agencies
@@ -7,5 +9,39 @@ class Company < ApplicationRecord
   
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true
+
+  def self.csv_upload_companies
+    # if FileTest.exist?( Rails.root.to_s + '/retenidos_con_pedido_cantidad.csv' )
+    #   File.delete( Rails.root.to_s + '/retenidos_con_pedido_cantidad.csv' )
+    # end
+
+    #data_csv = Base64.decode64(params[:csv]['data:text/csv;base64,'.length .. -1])
+    #File.open("retenidos_con_pedido_cantidad.csv","wb") {|f| f.write(data_csv) }
+    csv_content = CSV.read(Rails.root.to_s + '/companies.csv')
+
+    errors = []
+    added_items = 0
+    csv_content.each_with_index do |row, i|
+      next if i == 0
+
+      company_name = row[0]
+      brand_name = row[1]
+
+      company = Company.find_by_name( company_name )
+      if ! company.present?
+        company = Company.create( :name => company_name )
+      end
+
+      brand = Brand.find_by_name( brand_name )
+      if ! brand.present?
+        brand = Brand.create( :name => brand_name, :company_id => company.id )
+        added_items += 1 
+      end
+    end
+
+    puts 'added_items: ' + added_items.to_s
+    #WarehouseMailer.csv_locate( 'miguel@pcuervo.com', added_items, errors ).deliver_now
+    return { added_items: added_items }
+  end
 
 end
